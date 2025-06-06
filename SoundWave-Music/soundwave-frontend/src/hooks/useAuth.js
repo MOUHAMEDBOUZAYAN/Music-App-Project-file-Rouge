6fd40173d831389dcc12268794f5c6371e95f3bd
mouhamedbouzayan <mohammedbouzi177@gmail.com>
2025-06-06 22:49:42 +0100

    Refactor useAuth hook to improve authentication context usage

--- SoundWave-Music/soundwave-frontend/src/hooks/useAuth.js
+++ SoundWave-Music/soundwave-frontend/src/hooks/useAuth.js
@@ -1,1 +1,15 @@
- 
+ import { useContext } from 'react';
+import AuthContext from '../store/AuthContext.jsx';
+
+// Hook personnalisé pour utiliser l'authentification
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  
+  if (!context) {
+    throw new Error('useAuth doit être utilisé dans un AuthProvider');
+  }
+  
+  return context;
+};
+
+export default useAuth;
