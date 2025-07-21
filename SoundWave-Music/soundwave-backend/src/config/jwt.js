7e0894b1324f43e3fe12ec6f5d5835ed736bdb0b
mouhamedbouzayan <mohammedbouzi177@gmail.com>
2025-07-21 13:01:18 +0100

    Update JWT secret, simplify HTML body class, and clean up CSS and JSX files by removing unused styles and components

--- SoundWave-Music/soundwave-backend/src/config/jwt.js
+++ SoundWave-Music/soundwave-backend/src/config/jwt.js
@@ -1,5 +1,5 @@
 module.exports = {
-  secret: process.env.JWT_SECRET || 'votre_cle_secrete_super_secrete',
+  secret: process.env.JWT_SECRET || 'Mouhamed12@',
   expiresIn: '7d', // Durée de validité du token (ex: 7 jours)
   issuer: 'soundwave-app', // Optionnel : identifiant de l'émetteur du token
 }; 
